ce7e3f10bbb2b60c50969b718136b7a18dc3f21e
lesguillemets <lesguillemets@users.noreply.github.com>
2016-11-18 07:26:06 +0900

    basic evaluations

--- src/LScheme/Eval.hs
+++ src/LScheme/Eval.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
 module LScheme.Eval where
 
 import LScheme.SchemeVal
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (unpack)
+import Data.Maybe
+import Data.List
+import qualified Data.Map.Strict as M
 
 eval :: SchemeVal -> SchemeVal
 eval v@(String _) = v
@@ -9,3 +15,30 @@ eval v@(Float _) = v
 eval v@(Number _) = v
 eval v@(Bool _) = v
 eval (List [Atom "quote", v]) = v
+eval (List (Atom f : args)) = apply f $ map eval args
+
+apply :: ByteString -> [SchemeVal] -> SchemeVal
+apply f = fromMaybe (const (Bool False)) $ M.lookup f primitives
+
+primitives :: M.Map ByteString ([SchemeVal] -> SchemeVal)
+primitives =
+    [ ("+", numericBinop (+))
+    , ("-", numericBinop (-))
+    , ("*", numericBinop (*))
+    , ("/", numericBinop div) -- TODO: handle float
+    , ("mod", numericBinop mod)
+    , ("quotient", numericBinop quot)
+    , ("remainder", numericBinop rem)
+    ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [SchemeVal] -> SchemeVal
+numericBinop op = Number . foldl1' op . map unpackNum
+
+unpackNum :: SchemeVal -> Integer
+unpackNum (Number n) = n
+-- TODO : not liking this
+unpackNum (String ns) = case reads . unpack $ ns of
+                             [] -> 0
+                             ((n,_):_) -> n
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0 -- whoa
--- test/Eval.hs
+++ test/Eval.hs
@@ -17,6 +17,8 @@ evalSpec = do
             eval (String "HI") `shouldBe` String "HI"
         it "handles quotes" $
             eval (List [Atom "quote", Atom "that"]) `shouldBe` Atom "that"
+        it "calculates (+ 2 2)" $
+            eval (List [Atom "+", Number 2, Number 2]) `shouldBe` Number 4
 
 -- TODO : relies on parsing, too. Where should I put the tests?
 evaLoopSpec :: Spec
@@ -24,6 +26,12 @@ evaLoopSpec = do
     describe "evaLoop" $ do
         it "evals 'atom as atom" $
             evPs "'atom" `shouldBe` Right (Atom "atom")
+        it "currently treats (+ 2 (-4 1)) as (+ 2 0) then 2" $
+            evPs "(+ 2 (-4 1))" `shouldBe` Right (Number 2)
+        it "evals (+ 2 (- 4 1))" $
+            evPs "(+ 2 (- 4 1))" `shouldBe` Right (Number 5)
+        it "(- (+ 4 6 3) 3 5 2)" $
+            evPs "(- (+ 4 6 3) 3 5 2)" `shouldBe` Right (Number 3)
         where
             evPs x = eval <$> (pt parseExpr x)
 

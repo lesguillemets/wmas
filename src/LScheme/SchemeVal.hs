4def8c430ece4b86127d3d1994a19ee26a777c9f
lesguillemets <lesguillemets@users.noreply.github.com>
2016-11-17 12:35:04 +0900

    showVal

--- src/LScheme/SchemeVal.hs
+++ src/LScheme/SchemeVal.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module LScheme.SchemeVal
     ( SchemeVal (..)
+    , showVal
     )
     where
 
+import Prelude hiding (unwords)
 import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (unwords, pack)
+import Data.Monoid
 
 data SchemeVal = Atom ByteString
                | List [SchemeVal]
@@ -13,3 +18,15 @@ data SchemeVal = Atom ByteString
                | String ByteString
                | Character Char
                | Bool Bool deriving (Show, Eq)
+
+showVal :: SchemeVal -> ByteString
+showVal (Atom name) = name
+showVal (List ls) = "(" <> (unwords . map showVal $ ls) <> ")"
+showVal (DottedList h t) =
+    "(" <> (unwords . map showVal $ h) <> " . " <> showVal t <> ")"
+showVal (Number n) = pack . show $ n
+showVal (Float x) = pack . show $ x
+showVal (String s) = s
+showVal (Character c) = "#\\" <> (pack . return $ c)
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"

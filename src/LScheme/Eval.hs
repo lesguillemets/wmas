44f512f57ddbdb584f3ab0f4328ab307f8c6d73a
lesguillemets <lesguillemets@users.noreply.github.com>
2016-11-18 13:35:33 +0900

    type testing
    needs quote and eval

--- src/LScheme/Eval.hs
+++ src/LScheme/Eval.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLists #-}
-module LScheme.Eval where
+module LScheme.Eval
+    ( eval
+    , apply
+    ) where
 
 import LScheme.SchemeVal
 import Data.ByteString (ByteString)
@@ -15,10 +18,10 @@ eval v@(Float _) = v
 eval v@(Number _) = v
 eval v@(Bool _) = v
 eval (List [Atom "quote", v]) = v
-eval (List (Atom f : args)) = apply f $ map eval args
+eval (List (Atom func : args)) = apply func $ map eval args
 
 apply :: ByteString -> [SchemeVal] -> SchemeVal
-apply f = fromMaybe (const (Bool False)) $ M.lookup f primitives
+apply func = fromMaybe (const (Bool False)) $ M.lookup func primitives
 
 primitives :: M.Map ByteString ([SchemeVal] -> SchemeVal)
 primitives =
@@ -29,10 +32,46 @@ primitives =
     , ("mod", numericBinop mod)
     , ("quotient", numericBinop quot)
     , ("remainder", numericBinop rem)
+    , ("symbol?", unaryOp isSymbol)
+    , ("integer?", unaryOp isInteger)
+    , ("number?", unaryOp isNumber)
+    , ("boolean?", unaryOp isBool)
+    , ("string?", unaryOp isString)
+    , ("list?", unaryOp isList)
+    , ("char?", unaryOp isCharacter)
     ]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [SchemeVal] -> SchemeVal
 numericBinop op = Number . foldl1' op . map unpackNum
+
+-- TODO : error handling
+unaryOp :: (SchemeVal -> SchemeVal) -> [SchemeVal] -> SchemeVal
+unaryOp func [x] = func x
+isSymbol :: SchemeVal -> SchemeVal
+isSymbol (Atom _) = t
+isSymbol _ = f
+isList :: SchemeVal -> SchemeVal
+isList (List _) = t
+isList (DottedList _ _) = t
+isList _ = f
+-- FIXME : Number and Integer
+isInteger (Number _) = t
+isInteger _ = f
+isNumber (Number _) = t
+isNumber (Float _) = t
+isNumber _ = f
+isString :: SchemeVal  -> SchemeVal
+isString (String _) = t
+isString _ = f
+isCharacter :: SchemeVal  -> SchemeVal
+isCharacter (Character _) = t
+isCharacter _ = f
+isBool :: SchemeVal  -> SchemeVal
+isBool (Bool _) = t
+isBool _ = f
+
+t = Bool True
+f = Bool False
 
 unpackNum :: SchemeVal -> Integer
 unpackNum (Number n) = n
--- test/Eval.hs
+++ test/Eval.hs
@@ -20,6 +20,41 @@ evalSpec = do
         it "calculates (+ 2 2)" $
             eval (List [Atom "+", Number 2, Number 2]) `shouldBe` Number 4
 
+    describe "type-testing" $ do
+        it "works for symbol?" $
+            typeTest "symbol?" `shouldBe` [Atom ""]
+        it "works for list?" $ do
+            typeTest "list?" `shouldBe` [List [], DottedList [] (Atom "")]
+        it "works for integer?" $
+            typeTest "integer?" `shouldBe` [Number 0]
+        it "works for number?" $
+            typeTest "number?" `shouldBe` [Number 0, Float 0]
+        it "works for string?" $
+            typeTest "string?" `shouldBe` [String ""]
+        it "works for char?" $
+            typeTest "char?" `shouldBe` [Character  ' ']
+        it "works for boolean?" $
+            typeTest "boolean?" `shouldBe` [Bool True]
+
+    where
+        typeTest test =
+            map eval $
+                filter (isTrue . eval . (\e -> List [Atom test, e])) vals
+        vals :: [SchemeVal]
+        vals = map quote [ Atom ""
+                         , List []
+                         , DottedList [] (Atom "")
+                         , Number 0
+                         , Float 0
+                         , String ""
+                         , Character ' '
+                         , Bool True
+                         ]
+        quote a = List [Atom "quote", a]
+        isTrue :: SchemeVal -> Bool
+        isTrue (Bool True) = True
+        isTrue (Bool False) = False
+
 -- TODO : relies on parsing, too. Where should I put the tests?
 evaLoopSpec :: Spec
 evaLoopSpec = do
@@ -32,6 +67,7 @@ evaLoopSpec = do
             evPs "(+ 2 (- 4 1))" `shouldBe` Right (Number 5)
         it "(- (+ 4 6 3) 3 5 2)" $
             evPs "(- (+ 4 6 3) 3 5 2)" `shouldBe` Right (Number 3)
+
         where
             evPs x = eval <$> (pt parseExpr x)
 
